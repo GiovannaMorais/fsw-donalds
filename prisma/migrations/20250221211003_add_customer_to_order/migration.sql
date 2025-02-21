9da5605ec95cb4f2dc83aee413f2792078cdabe1
Giovanna Morais <gigimorais9@hotmail.com>
2025-02-21 18:37:40 -0300

    feat: ensure order can be created

---
+++ prisma/migrations/20250221211003_add_customer_to_order/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - Added the required column `customerCpf` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `customerName` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "MenuCategory" DROP CONSTRAINT "MenuCategory_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_menuCategoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_restaurantId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "customerCpf" TEXT NOT NULL,
+ADD COLUMN     "customerName" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "MenuCategory" ADD CONSTRAINT "MenuCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_menuCategoryId_fkey" FOREIGN KEY ("menuCategoryId") REFERENCES "MenuCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-
 import "./globals.css";
 
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import { CartProvider } from "./[slug]/menu/contexts/cart";
+
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
@@ -23,11 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
-      <CartProvider>
-        {children}
-      </CartProvider>
-      </body>
+        <CartProvider>{children}</CartProvider>
 
+        <Toaster />
+      </body>
     </html>
   );
 }

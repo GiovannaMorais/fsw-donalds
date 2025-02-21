8306a18f07072e312f47d7880f0cad9cf14685d1
Giovanna Morais <gigimorais9@hotmail.com>
2025-02-21 17:36:16 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-export interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+export interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
@@ -12,25 +13,26 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
-
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
 });
 
-
-export const CartProvider = ({ children } : {children: ReactNode}) => {
+export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const toggleCart = () => setIsOpen((prev) => !prev);
-
+  const toggleCart = () => {
+    setIsOpen((prev) => !prev);
+  };
   const addProduct = (product: CartProduct) => {
-   const productIsAlreadyOnTheCart = products.some(
+    const productIsAlreadyOnTheCart = products.some(
       (prevProduct) => prevProduct.id === product.id,
     );
     if (!productIsAlreadyOnTheCart) {
@@ -48,10 +50,30 @@ export const CartProvider = ({ children } : {children: ReactNode}) => {
       });
     });
   };
-
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity - 1 };
+      });
+    });
+  };
   return (
-    <CartContext.Provider value={{ isOpen, products, toggleCart, addProduct}}>
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+        decreaseProductQuantity,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
-}
+};

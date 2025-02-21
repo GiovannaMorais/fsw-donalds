cd483e4cc1b29fa984fcc3f2e5a575d44853dc60
Giovanna Morais <gigimorais9@hotmail.com>
2025-02-21 19:35:31 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,19 +1,21 @@
-'use client'
-import { Product } from '@prisma/client'
-import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+"use client";
 
-import { Button } from '@/components/ui/button'
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
-  product: Pick<Product,  'name' | 'imageUrl'>
-  }
+  product: Pick<Product, "name" | "imageUrl">;
+}
 
- const ProductHeader = ({ product }: ProductHeaderProps) => {
-  const router = useRouter()
-  const handleBackClick = () => router.back()
-
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
     <div className="relative min-h-[300px] w-full">
       <Button
@@ -25,15 +27,23 @@ interface ProductHeaderProps {
         <ChevronLeftIcon />
       </Button>
 
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
+
       <Button
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
-      <Image src={product.imageUrl} alt={product.name} fill className="object-contain" />
     </div>
-  )
-}
-export default ProductHeader
+  );
+};
+
+export default ProductHeader;
